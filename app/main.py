af14a3ad44c13dd686b79b8968359f4f4833cc47
Blake <blakepomeroy03@gmail.com>
2026-04-16 16:44:57 -0600

    initial commit: basic database/file upload

---
+++ app/main.py
@@ -0,0 +1,45 @@
+import os
+from fastapi import FastAPI, UploadFile, File, Depends
+from app.db import engine, Base, SessionLocal
+import app.models as models
+from sqlalchemy.orm import Session
+
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+os.makedirs("uploads", exist_ok = True)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    file_location = f"uploads/{file.filename}"
+
+    with open(file_location, "wb") as buffer:
+        buffer.write(await file.read())
+
+    size = os.path.getsize(file_location)
+
+    new_record = models.FileRecord(
+        filename = file.filename,
+        filepath = file_location,
+        size = size
+    )
+
+    db.add(new_record)
+    db.commit()
+    db.refresh(new_record)
+    return {"message": "Success", "id": new_record.id}
---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.db import Base
+
+class FileRecord(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, index=True)
+    filepath = Column(String, unique=True)
+    size = Column(Integer)
