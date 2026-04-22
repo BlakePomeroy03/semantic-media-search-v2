ba30e6c814d39abe786c7441e8a72b516c091dd6
Blake <blakepomeroy03@gmail.com>
2026-04-22 13:08:57 -0600

    feat: Changed CLIP model and adding vector caching

--- app/main.py
+++ app/main.py
@@ -3,14 +3,12 @@ import io
 import json
 from fastapi import FastAPI, UploadFile, File, Depends, Request
 from PIL import Image
-from app.db import engine, Base, SessionLocal
+from app.db import engine, Base, SessionLocal,get_db
 import app.models as models
 from sqlalchemy.orm import Session
 from contextlib import asynccontextmanager
 from sentence_transformers import SentenceTransformer, util
-
-
-app = FastAPI()
+from app.routes import search
 
 Base.metadata.create_all(bind=engine)
 
@@ -19,27 +17,40 @@ os.makedirs("uploads", exist_ok = True)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Loading Model")
+    app.state.model = SentenceTransformer('clip-ViT-L-14')
+    print("Preparing Cache")
+    db = SessionLocal()
 
-    app.state.model = SentenceTransformer('clip-ViT-B-32')
+    try:
+        all_files = db.query(models.FileRecord).all()
 
-    print("Done")
+        cache_filenames = []
+        cache_vectors = []
+        count = 0
+        for file_record in all_files:
+            cache_filenames.append(file_record.filename)
+
+            vector = json.loads(file_record.embedding)
+            cache_vectors.append(vector)
+
+            app.state.filenames = cache_filenames
+            app.state.vectors = cache_vectors
+
+            count = count + 1
+
+    finally:
+        print(f"Cache warmed with {count} vectors.")
+        db.close()
 
     yield
 
 app = FastAPI(lifespan = lifespan)
 
+app.include_router(search.router)
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-    
 
 @app.post("/upload")
 async def upload_file(request: Request, file: UploadFile, db: Session = Depends(get_db)):
@@ -80,22 +91,3 @@ async def upload_file(request: Request, file: UploadFile, db: Session = Depends(
 async def list_files(db: Session = Depends(get_db)):
     files = db.query(models.FileRecord).all()
     return files
-
-
-@app.get("/search")
-async def search_files(query: str, request: Request, db: Session = Depends(get_db)):
-    model = request.app.state.model
-    query_embedding = model.encode([query])[0]
-
-    all_files = db.query(models.FileRecord).all()
-
-    results = []
-
-    for file_record in all_files:
-        image_embedding = json.loads(file_record.embedding)
-        score = util.cos_sim(image_embedding, query_embedding)[0][0].item()
-        results.append({"filename": file_record.filename, "score": score})
-
-
-    results.sort(key=lambda x: x["score"], reverse = True)
-    return results[:3]
